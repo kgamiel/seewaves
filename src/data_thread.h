07031afd6df2615eb6755b980179fba14971d3d0
kgamiel <kgamiel@islandedge.com>
2013-12-11 11:35:02 +0100

    rearranging code

---
+++ src/data_thread.h
@@ -0,0 +1,13 @@
+/*
+ * data_thread.h
+ *
+ *  Created on: Dec 10, 2013
+ *      Author: kgamiel
+ */
+
+#ifndef DATA_THREAD_H_
+#define DATA_THREAD_H_
+
+void *data_thread_main(void *user_data);
+
+#endif /* DATA_THREAD_H_ */
